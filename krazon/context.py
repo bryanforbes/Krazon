1f9e543bdf07e6e66583df61b6ffc7dec392bee0
Bryan Forbes <bryan@reigndropsfall.net>
2018-09-06 13:04:10 -0500

    Updates

--- krazon/context.py
+++ krazon/context.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Optional, Union, List
 from datetime import datetime
 from mypy_extensions import TypedDict
--- krazon/krazon.py
+++ krazon/krazon.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import List, Optional, Any, Dict, cast
 from pathlib import Path
 from io import BytesIO
