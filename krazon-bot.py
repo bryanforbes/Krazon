6a6aa11126597a11739e669bd2568d74fe9b86e2
Bryan Forbes <bryan@reigndropsfall.net>
2018-10-17 10:26:20 -0500

    Update deps and change bot runner invocation

--- krazon-bot.py
+++ krazon-bot.py
@@ -3,11 +3,12 @@
 import asyncio
 import uvloop
 
-from botus_receptus import run
+from botus_receptus import cli
 from krazon import Krazon
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 if __name__ == '__main__':
-    run(Krazon, './config.ini')
+    runner = cli(Krazon, './config.ini')
+    runner()
